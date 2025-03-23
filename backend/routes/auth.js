61a529b3c2bcd40ae0e75d23a396c96f52cee769
Jack <jackangusg@gmail.com>
2025-03-23 15:22:05 -0500

    Add status code comments

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -12,7 +12,7 @@ router.post('/register', async (req, res) => {
     try {
         const existingUser = await User.findOne({email});
         if (existingUser) {
-            return res.status(400).json({ error: 'There is already a registered user under this email.' });
+            return res.status(400).json({ error: 'There is already a registered user under this email.' }); // 400 is bad request
         }
 
         const newUser = await User.create({ email, password });
@@ -20,9 +20,9 @@ router.post('/register', async (req, res) => {
         delete pojoUser.password; // remove password from response for security
         delete pojoUser.__v; // remove __v because its unnecessary
 
-        res.status(201).json({ user: pojoUser });
+        res.status(201).json({ user: pojoUser }); // 201 is created
     } catch (error) {
-        res.status(500).json({ error: 'Error registering new user' });
+        res.status(500).json({ error: 'Error registering new user' }); // 500 is internal server error
     }
 });
 
@@ -32,18 +32,18 @@ router.post('/login', async (req, res) => {
     try {
         const user = await User.findOne({ email });
         if (!user) {
-            return res.status(404).json({ error: 'This email is not registered.' });
+            return res.status(404).json({ error: 'This email is not registered.' }); // 404 is not found
         }
 
         const passwordMatch = await bcrypt.compare(password, user.password);
         if (!passwordMatch) {
-            return res.status(401).json({ error: 'Incorrect password' });
+            return res.status(401).json({ error: 'Incorrect password' }); // 401 is unauthorized
         }
 
         const token = jwt.sign({ userId: user._id }, process.env.TOKEN_SECRET, {expiresIn: '1d'}); // grant the user a token valid for one day
         res.json({ token });
     } catch (error) {
-        res.status(500).json({ error: 'Error logging in user' });
+        res.status(500).json({ error: 'Error logging in user' }); // 500 is internal server error
     }
 });
 
